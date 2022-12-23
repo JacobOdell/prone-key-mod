e8838b852d2132152df60a30bd5b2c4b37f847dd
Jacob <jacob.eugene.odell@gmail.com>
2022-12-23 15:22:36 -0800

    Initial commit

---
+++ src/main/java/com/pronekey/pronekeymod/ProneKeyMod.java
@@ -0,0 +1,10 @@
+package com.pronekey.pronekeymod;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ProneKeyMod implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/com/pronekey/pronekeymod/client/ProneKeyModClient.java
@@ -0,0 +1,13 @@
+package com.pronekey.pronekeymod.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class ProneKeyModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
