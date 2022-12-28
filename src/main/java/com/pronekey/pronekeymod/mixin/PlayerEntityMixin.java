4c17900919e102bd5f63d7eb0c866420689cc656
Redland123 <David.50mg@gmail.com>
2022-12-28 13:52:44 -0800

    Changed key to x for testing and updated required loader version for server compatibility
    (cherry picked from commit 9149e7bfabc23784b4ff6d62241988786c6a19f5)

---
+++ src/main/java/com/pronekey/pronekeymod/mixin/PlayerEntityMixin.java
@@ -0,0 +1,19 @@
+package com.pronekey.pronekeymod.mixin;
+
+import com.pronekey.pronekeymod.client.ProneKeyModClient;
+import net.minecraft.entity.player.PlayerEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(PlayerEntity.class)
+public class PlayerEntityMixin {
+    @Inject(method = "isSwimming", at = @At("HEAD"), cancellable = true)
+    public void isSwimming(CallbackInfoReturnable<Boolean> cir) {
+        if(ProneKeyModClient.isProneKeyPressed){
+            cir.setReturnValue(true);
+            //((PlayerEntity)(Object)this)
+        }
+    }
+}
